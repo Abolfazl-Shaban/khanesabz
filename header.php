bad788500a3895f16573d1f469cf9224872f7af4
Pc School <abolfazlshaban2008@gmail.com>
2025-04-29 08:47:25 +0330

    Initial Project

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class="flex w-full justify-between items-center mt-4">
+    <div></div>
+    <div class="w-1/3"></div>
+    <nav class="flex gap-8 justify-between items-center w-1/3">
+        <a class="bg-green-500 rounded-full py-2 px-4 text-white flex items-center justify-center">صفحه اصلی</a>
+        <a class="bg-green-500 rounded-full py-2 px-4 text-white flex items-center justify-center">فروشگاه</a>
+        <a class="bg-green-500 rounded-full py-2 px-4 flex text-white items-center justify-center">فروشنده شو</a>
+    </nav>
+    <div class="flex justify-center mx-4 w-1/3">
+        <button class="bg-green-200  rounded-full p-2 px-3  pl-9">ورود</button>
+        <button class=" bg-green-500 rounded-full p-2 px-3 text-white -mr-8" >ثبت نام</button>
+    </div>
+</div> 
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl" >
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>خانه سبز</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body class="h-screen bg-yellow-50 font-[IRANSans]">
+    <?php include "header.php" ?> 
+</body>
+<script src="tailwind.js">
+</script>
+
+</html>
